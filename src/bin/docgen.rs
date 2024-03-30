cef33782a3eb2a347266960f33d89853740ff18d
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2024-03-30 15:49:30 -0700

    partial-feat: some rust + some docgen

--- src/bin/docgen.rs
+++ src/bin/docgen.rs
@@ -1,18 +1,8 @@
-extern crate mdbook;
-use mdbook::MDBook;
 use std::error::Error;
+use openpid::prelude::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    std::fs::create_dir_all("outputs/book");
-
-    let mut cfg = mdbook::config::Config::default();
-
-    cfg.book.title = Some("Sensor Title".to_string());
-
-    MDBook::init("outputs/book")
-        .with_config(cfg)
-        .build()?;
-
-    std::fs::write("outputs/book/image.svg", openpid::docgen::generate_packet_diagram("Packet Format".to_owned(), vec![("Size".to_owned(), Some(8)), ("FrameID".to_owned(), Some(8)),("Payload".to_owned(), None), ("Crc".to_owned(), Some(16))]))?;
+    let spec: OpenPID = toml::from_str(&std::fs::read_to_string("./openpid.toml")?)?;
+    openpid::docgen::document(&spec, std::path::PathBuf::from("./outputs"))?;
     Ok(())
 }
